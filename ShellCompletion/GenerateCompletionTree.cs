4884ccb965a04d32b51dede535903fb68442c398
Alexander Larsen <alexander.larsen@keysight.com>
2022-06-13 13:12:07 +0200

    Initial commit

---
+++ ShellCompletion/GenerateCompletionTree.cs
@@ -0,0 +1,14 @@
+using System.Threading;
+using OpenTap.Cli;
+
+namespace ShellCompletion
+{
+    public class GenerateCompletionTree : ICliAction
+    {
+        public int Execute(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
