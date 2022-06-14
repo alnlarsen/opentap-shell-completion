95fcf6f96562897dcfa12b9e3d6c683ac03b0a31
Alexander Larsen <alexander.larsen@keysight.com>
2022-06-14 14:51:10 +0200

    wip

--- ShellCompletion/GenerateCompletionTree.cs
+++ ShellCompletion/GenerateCompletionTree.cs
@@ -1,13 +1,18 @@
 using System.Threading;
 using OpenTap.Cli;
+using OpenTap;
 
 namespace ShellCompletion
 {
+    [Display("regenerate", Description: "Regenerate the completion tree for this installation.", Groups: new[] { "completion" })]
     public class GenerateCompletionTree : ICliAction
     {
         public int Execute(CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+          var actions =           TypeData.GetDerivedTypes<ICliAction>();
+
+
+          return 0;
         }
     }
 }
