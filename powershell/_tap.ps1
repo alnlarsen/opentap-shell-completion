ff33581b9f78bba263d6604329671ec91a7616cf
allarsen <alexander.larsen@keysight.com>
2022-06-27 17:37:18 +0200

    Improve contextual completion

--- powershell/_tap.ps1
+++ powershell/_tap.ps1
@@ -1,8 +1,28 @@
+function GetWordNumber {
+    param(
+        [string[]] $arguments,
+        [int] $cursorPosition
+    )
+    
+    $cnt = 0;
+    $i = 0;
+    foreach ($arg in $arguments){
+        $i = $i + 1
+        if (($cnt + $arg.Length + 1) -ge $cursorPosition){
+            return $i
+        }
+        $cnt += $arg.Length
+    }
+    return $arguments.Length + 1
+}
+
 function Complete {
     param(
-        [string[]] $arguments
+        [string[]] $arguments,
+        [int] $cursorPosition
     )
     $a = $arguments[0]
+    $current = GetWordNumber $arguments $cursorPosition
     $location = (Get-Childitem (Get-Command $a).Source).Directory.FullName
     $jsonFile = "$location/.tap-completions.json"
 
@@ -10,8 +30,12 @@ function Complete {
 
     $json = ConvertFrom-Json (Get-Content $jsonFile)
 
+    $i = 0
     $rest = $arguments[1..$arguments.Length]
     foreach ($r in $rest) {
+        $i += 1
+        if ($i -ge $current) { break; }
+            
         $cand = $json.Completions
         foreach ($c in $cand){
             if ($c.Name -eq $r)
@@ -41,7 +65,7 @@ function Complete {
 $scriptBlock = {
     param($wordToComplete, $commandAst, $cursorPosition)    
 
-    Complete $commandAst.CommandElements | 
+    Complete $commandAst.CommandElements $cursorPosition | 
     Where-Object { $_ -like "$wordToComplete*" } |
     Foreach-Object {         
         $_.Contains(' ') ? "`"$_`" " : "$_ "
