8f7b6b60dad986d19c7d9e0b19a3e701488b9b7d
allarsen <alexander.larsen@keysight.com>
2022-06-28 14:58:09 +0200

    Improve contextual powershell completion

--- powershell/_tap.ps1
+++ powershell/_tap.ps1
@@ -1,55 +1,68 @@
-function GetWordNumber {
-    param(
-        [string[]] $arguments,
-        [int] $cursorPosition
-    )
-    
-    $cnt = 0;
-    $i = 0;
-    foreach ($arg in $arguments){
-        $i = $i + 1
-        if (($cnt + $arg.Length + 1) -ge $cursorPosition){
-            return $i
-        }
-        $cnt += $arg.Length
-    }
-    return $arguments.Length + 1
-}
-
 function Complete {
     param(
         [string[]] $arguments,
-        [int] $cursorPosition
+        [int] $cursorPosition,
+        [string] $wordToComplete
     )
     $a = $arguments[0]
-    $current = GetWordNumber $arguments $cursorPosition
     $location = (Get-Childitem (Get-Command $a).Source).Directory.FullName
+    $packageXml = "$location/Packages/ShellCompletion/package.xml"
     $jsonFile = "$location/.tap-completions.json"
 
-    if (! (Test-Path $jsonFile)) { return }        
+    if (! (Test-Path $packageXml)) { return }        
+    if (! (Test-Path $jsonFile)) {
+        & "$location/tap" completion regenerate | Out-Null
+    }
 
     $json = ConvertFrom-Json (Get-Content $jsonFile)
 
     $i = 0
-    $rest = $arguments[1..$arguments.Length]
-    foreach ($r in $rest) {
+    $idx = $arguments.IndexOf($wordToComplete)
+    if ($idx -eq -1) { $idx = $arguments.Count }
+    $rest = $arguments[1..$arguments.Count + 1]
+    foreach ($r in $rest) {        
+        if ($i -gt $idx) { break; }
         $i += 1
-        if ($i -ge $current) { break; }
             
         $cand = $json.Completions
-        foreach ($c in $cand){
-            if ($c.Name -eq $r)
-            {
+        foreach ($c in $cand) {
+            if ($c.Name -eq $r) {
                 $json = $c
                 break;
             }
         }
     }
+    
+    $prev = $idx - 1
+    $previousWord = $null
+    if ($prev -ge 0 -and $prev -lt $arguments.Count) {
+        $previousWord = $arguments[$prev]
+    }
+
     $completions = [System.Collections.Generic.List[string]]::new()
-    foreach ($comp in $json.Completions){
+
+    if ($previousWord -and $previousWord.StartsWith("-")) {
+        foreach ($f in $json.FlagCompletions) {
+            if ("-$($f.ShortName)" -eq $previousWord -or "--$($f.LongName)" -eq $previousWord) {
+                if ($f.Type -eq "System.Boolean") {
+                    break;
+                }
+
+                if ($f.SuggestedCompletions) {
+                    foreach ($s in $f.SuggestedCompletions) {
+                        $completions.Add($s)
+                    }
+                }
+                return $completions;
+            }
+        }
+    }    
+    
+    foreach ($comp in $json.Completions) {
         $completions.Add($comp.Name)
     }
-    foreach ($comp in $json.FlagCompletions){
+
+    foreach ($comp in $json.FlagCompletions) {
         $sn = $comp.ShortName
         if (! [String]::IsNullOrWhiteSpace($sn) ) {
             $completions.Add("-$sn")
@@ -65,7 +78,7 @@ function Complete {
 $scriptBlock = {
     param($wordToComplete, $commandAst, $cursorPosition)    
 
-    Complete $commandAst.CommandElements $cursorPosition | 
+    Complete $commandAst.CommandElements $cursorPosition $wordToComplete | 
     Where-Object { $_ -like "$wordToComplete*" } |
     Foreach-Object {         
         $_.Contains(' ') ? "`"$_`" " : "$_ "
